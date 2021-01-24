9c68562304b333eaf099c509ede6686b23ffd0bb
carlling24 <LingHuan5593@qq.com>
2021-01-24 17:50:24 +0800

    code read init

---
+++ design-patterns/src/main/java/com/code/read/pattern/PatternApplication.java
@@ -0,0 +1,13 @@
+package com.code.read.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternApplication.class, args);
+    }
+
+}
